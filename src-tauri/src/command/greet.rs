4e13ba091d73b74788f5a1069cc85540ad79feac
Akihiro Hamana <doarat@gmail.com>
2025-04-09 22:54:58 +0900

    テストコードの構成を変更

--- src-tauri/src/command/greet.rs
+++ src-tauri/src/command/greet.rs
@@ -2,3 +2,14 @@
 pub fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::greet;
+
+    #[test]
+    fn test_greet() {
+        let result = greet("Tauri");
+        assert_eq!(result, "Hello, Tauri! You've been greeted from Rust!");
+    }
+}
--- src-tauri/tests/greet_test.rs
+++
@@ -1,7 +0,0 @@
-use tauri_app_lib::command::greet::greet;
-
-#[test]
-fn test_greet() {
-    let result = greet("Tauri");
-    assert_eq!(result, "Hello, Tauri! You've been greeted from Rust!");
-}
