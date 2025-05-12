c2d0677090f1b18948979be03b893189ec0ba53e
Akihiro Hamana <doarat@gmail.com>
2025-05-12 22:31:50 +0900

    lint-staged の設定を TypeScript に恩恵を受けられる形式に変更

--- lint-staged.config.ts
+++ lint-staged.config.ts
@@ -1,3 +1,7 @@
-export default {
+import type { Configuration } from "lint-staged";
+
+const config: Configuration = {
   "*": "eslint --flag unstable_native_nodejs_ts_config --fix",
 };
+
+export default config;
